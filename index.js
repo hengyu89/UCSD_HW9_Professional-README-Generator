8a3875b308ea34a09a5912c0021898cc6695f954
Heng Yu <heng.yu161@gmail.com>
2022-05-22 12:55:37 +0800

    Construct a layout of readme file

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'test',
+            message: "Aloha",
+        },
+    ]);
+};
+
+promptUser();
